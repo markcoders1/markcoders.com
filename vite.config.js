52d1d8c3e2ff38290e61a29e7efe733fcb6d4401
Mir <143105008+markcoders1@users.noreply.github.com>
2026-08-29 02:52:07 +0500

    base updated

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   // GitHub Pages project site: https://<user>.github.io/Markcoders/
-  base: '/Markcoders/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
